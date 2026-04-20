fcd4001bdedefb5fbba9c089d07fb9a4b2f2ef43
Princess Wilson <68427973+princess2wilson@users.noreply.github.com>
2026-04-20 15:45:04 +0100

    Add: 3sum-smaller - submission-0

---
+++ Data Structures & Algorithms/3sum-smaller/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        
+        count = 0 
+        nums.sort()
+
+        for i in range(len(nums)-2):
+            l = i+1
+            r = len(nums)-1
+
+            while l<r:
+                if nums[i]+nums[l]+nums[r]<target:
+                    count+=r-l
+                    l+=1
+                else:
+                    r-=1
+        return count
+                    
+            

6b35ad24e1a4b04a98a3bb0d00081c06d0b69dc4
Princess Wilson <68427973+princess2wilson@users.noreply.github.com>
2026-05-04 17:21:24 +0100

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = []
+
+        for start,end in intervals:
+            if res and res[-1][1] >= start :
+                res[-1][1]=max(res[-1][1],end)
+            else:
+                res.append([start,end])
+
+        return res
+
+                
+
+            
+        

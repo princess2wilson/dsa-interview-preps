7bdbf5e35d7217de10e8f75d723ee55dc7c9d4dc
Princess Wilson <68427973+princess2wilson@users.noreply.github.com>
2026-05-20 14:19:22 +0100

    Add: random-pick-with-weight - submission-6

---
+++ Data Structures & Algorithms/random-pick-with-weight/submission-6.py
@@ -0,0 +1,42 @@
+import random
+from bisect import bisect_left
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = w
+        self.cm_sum = []
+        total = 0
+        for num in self.w:
+            total+=num
+            self.cm_sum.append(total)
+        self.total = total
+        
+
+    def pickIndex(self) -> int:
+        random_number = random.randint(1,self.total)
+        l = 0
+        r = len(self.cm_sum) -1 
+
+        while l<r:
+            mid = (l+r) // 2
+
+            if self.cm_sum[mid] < random_number:
+                l = mid+1
+            else:
+                r = mid 
+        return r
+            
+            
+        
+
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
+#[0,1,3,6,]
+#[1,2,3,4,5] 1 is the weight of 0th index
+#pick random index in range 0,4
+#prob of picking index 2 is 3/15
